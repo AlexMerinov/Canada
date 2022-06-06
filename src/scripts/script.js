85b36b1aac2423011892668c0f62b80a8b6d1c6e
John Doe <johndoe@example.com>
2022-06-06 16:19:20 +0300

    add section popular and slider

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,2 +1,48 @@
+const popularSlider = new Swiper('.popular-slider', {
 
+   slidesPerView: 2,
+   spaceBetween: 20,
+
+   // If we need pagination
+   pagination: {
+      clickable: true,
+      el: '.popular-slider-pagination',
+   },
+
+   breakpoints: {
+      576: {
+         slidesPerView: 3,
+      },
+
+      1236: {
+         slidesPerView: 4,
+         spaceBetween: 30
+      },
+   },
+ 
+ });
+
+ const slider = new Swiper('.slider', {
+
+   slidesPerView: 2,
+   spaceBetween: 20,
+
+   // If we need pagination
+   pagination: {
+      clickable: true,
+      el: '.slider-pagination',
+   },
+
+   breakpoints: {
+      576: {
+         slidesPerView: 3,
+      },
+
+      1236: {
+         slidesPerView: 4,
+         spaceBetween: 30
+      },
+   },
+ 
+ });
 
