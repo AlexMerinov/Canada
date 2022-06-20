4c5426f85ab895ebff148113ec89e6bd39a622d0
John Doe <johndoe@example.com>
2022-06-20 14:43:31 +0300

    add pagination for sliders & edit links for page about*

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -20,29 +20,45 @@ const popularSlider = new Swiper('.popular-slider', {
       },
    },
  
- });
-
- const slider = new Swiper('.slider', {
-
-   slidesPerView: 2,
-   spaceBetween: 20,
-
-   // If we need pagination
-   pagination: {
-      clickable: true,
-      el: '.slider-pagination',
-   },
-
-   breakpoints: {
-      576: {
-         slidesPerView: 3,
-      },
-
-      1280: {
-         slidesPerView: 4,
-         spaceBetween: 30
-      },
-   },
+});
  
- });
+const sliders = document.querySelectorAll('.slider__wrapper .slider');
+
+
+if (sliders) {
+   sliders.forEach(function (slider) {
+      const navigation = {
+         pagination: slider.closest('.slider__wrapper').querySelector('.slider-pagination')
+      }
+
+      new Swiper(slider, {
+
+         slidesPerView: 2,
+         spaceBetween: 20,
+      
+         // If we need pagination
+         pagination: {
+            clickable: true,
+            el: navigation.pagination,
+         },
+      
+         breakpoints: {
+            576: {
+               slidesPerView: 3,
+            },
+      
+            1280: {
+               slidesPerView: 4,
+               spaceBetween: 30
+            },
+         },
+       
+      });      
+
+   });
+}
+
+
+
+
 
